1c0d70d674f7942857c44d6310b624fc47595e38
angioma92 <77235118+angioma92@users.noreply.github.com>
2021-01-12 09:06:16 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,15 +1,13 @@
  
 #!/bin/bash
-timedatectl set-ntp true
-pacman -Syyu  --noconfirm
-echo ""
 read -p "Введите имя компьютера: " hostname
-echo ""
-echo " Используйте в имени только буквы латинского алфавита "
-echo ""
 read -p "Введите имя пользователя: " username
 
 echo $hostname > /etc/hostname
+echo 127.0.0.1 localhost > /etc/hosts
+echo ::1 localhost > /etc/hosts
+echo 127.0.1.1 $hostname.localdomain $hostname > /etc/hosts
+
 ln -sf /usr/share/zoneinfo/Asia/Irkutsk /etc/localtime
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
@@ -17,34 +15,28 @@ locale-gen
 echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf 
 echo "KEYMAP=ru" >> /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
-echo ""
+
 echo " Укажите пароль для ROOT "
 passwd
 
-echo ""
 useradd -m -G wheel,audio,video,storage,power -s /bin/bash $username
-echo ""
-echo 'Добавляем пароль для пользователя '$username' '
-echo ""
+echo 'Укажите пароль для пользователя '$username' '
 passwd $username
-echo ""
-
 clear
 
 bootctl install 
-clear
 echo ' default arch ' > /boot/loader/loader.conf
 echo 'title   ARCH LINUX' > /boot/loader/entries/arch.conf
 echo "linux  /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-
 pacman -S amd-ucode --noconfirm
 echo  'initrd /amd-ucode.img ' >> /boot/loader/entries/arch.conf
 echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo ""
-read -p "ROOT раздел для загрузчика: " root
+clear
+
+read -p "Укажите ROOT раздел для загрузчика (например: sda2): " root
 Proot=$(blkid -s PARTUUID /dev/$root | grep -oP '(?<=PARTUUID=").+?(?=")')
 echo options root=PARTUUID=$Proot rw quiet splash mitigations=off amdgpu.ppfeaturemask=0xffffffff >> /boot/loader/entries/arch.conf
-#
+
 cd /home/$username 
 git clone https://aur.archlinux.org/systemd-boot-pacman-hook.git
 chown -R $username:users /home/$username/systemd-boot-pacman-hook   
@@ -53,42 +45,24 @@ cd /home/$username/systemd-boot-pacman-hook
 sudo -u $username makepkg -si --noconfirm  
 rm -Rf /home/$username/systemd-boot-pacman-hook
 cd /home/$username 
-#
-clear
 mkinitcpio -p linux
-##########
-echo ""
 
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-clear
+
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
-clear
-echo ""
 
-pacman -Sy xorg-server xorg-drivers xorg-xdpyinfo mesa-demos --noconfirm
 pacman -Syy
-pacman -S gnome gnome-tweaks chrome-gnome-shell
-clear
+pacman -S xorg-server xorg-drivers xorg-apps mesa-demos --noconfirm
+pacman -S gnome gnome-tweaks chrome-gnome-shell --noconfirm
 pacman -S gdm --noconfirm
+
 systemctl enable gdm.service -f
-clear
 systemctl enable dhcpcd.service
-clear
-echo ""
-pacman -Sy pulseaudio alsa-utils alsa-lib pavucontrol pulseaudio-equalizer-ladspa   --noconfirm
 
-pacman -Sy exfat-utils ntfs-3g   --noconfirm
-#####
-clear
-
-pacman -Sy unzip unrar lha file-roller p7zip unace lrzip  --noconfirm  
-
-clear
-echo ""
-###############################################################################
+pacman -S pulseaudio alsa-utils alsa-lib pavucontrol pulseaudio-equalizer-ladspa --noconfirm
+pacman -S exfat-utils ntfs-3g unzip unrar lha file-roller p7zip unace lrzip --noconfirm
 pacman -S  git ttf-liberation ttf-dejavu htop neofetch --noconfirm
-clear
 
 cd /home/$username   
 git clone https://aur.archlinux.org/google-chrome.git
@@ -97,17 +71,6 @@ chown -R $username:users /home/$username/google-chrome/PKGBUILD
 cd /home/$username/google-chrome  
 sudo -u $username  makepkg -si --noconfirm  
 rm -Rf /home/$username/google-chrome
-clear
-
-cd /home/$username
-git clone https://aur.archlinux.org/yay.git
-chown -R $username:users /home/$username/yay
-chown -R $username:users /home/$username/yay/PKGBUILD 
-cd /home/$username/yay  
-sudo -u $username  makepkg -si --noconfirm  
-rm -Rf /home/$username/yay
-clear
-
 
 cd /home/$username 
 git clone https://aur.archlinux.org/inxi.git
@@ -116,7 +79,14 @@ chown -R $username:users /home/$username/inxi/PKGBUILD
 cd /home/$username/inxi  
 sudo -u $username  makepkg -si --noconfirm  
 rm -Rf /home/$username/inxi
-clear
+
+cd /home/$username
+git clone https://aur.archlinux.org/yay.git
+chown -R $username:users /home/$username/yay
+chown -R $username:users /home/$username/yay/PKGBUILD 
+cd /home/$username/yay  
+sudo -u $username  makepkg -si --noconfirm  
+rm -Rf /home/$username/yay
 
 cd /home/$username
  git clone https://aur.archlinux.org/pamac-aur.git
@@ -125,31 +95,16 @@ chown -R $username:users /home/$username/pamac-aur/PKGBUILD
 cd /home/$username/pamac-aur
 sudo -u $username  makepkg -si --noconfirm  
 rm -Rf /home/$username/pamac-aur
-clear
+
 pacman -S ufw --noconfirm
 systemctl enable ufw
-clear
+ufw enable
+
 pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
-clear
+
 pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
-clear
+
 pacman -Syu cabextract cups faudio lib32-acl lib32-faudio lib32-fontconfig lib32-freetype2 lib32-gettext lib32-giflib lib32-gnutls lib32-gst-plugins-base-libs lib32-gtk3 lib32-harfbuzz lib32-lcms2 lib32-libjpeg-turbo lib32-libldap lib32-libnl lib32-libpcap lib32-libpng lib32-libtasn1 lib32-libtiff lib32-libusb lib32-libxcomposite lib32-libxinerama lib32-libxrandr lib32-libxslt lib32-libxss lib32-mpg123 lib32-nspr lib32-nss lib32-opencl-icd-loader lib32-p11-kit lib32-sqlite lib32-v4l-utils lib32-vkd3d lib32-vulkan-icd-loader libimagequant lsof opencl-icd-loader python-distro python-evdev python-pillow sane vkd3d zenity icoutils xterm wget curl libudev0-shim python2 wxgtk-common wxgtk3 gnu-netcat lib32-libudev0-shim lib32-vulkan-intel lib32-vulkan-radeon python2-wxpython3 vulkan-intel vulkan-tools gamemode vulkan-radeon --noconfirm
+
 clear
-echo ""
-echo " Просмотрим//отредактируем /etc/fstab ?"
-while 
-    read -n1 -p  "1 - да, 0 - нет: " vm_fstab # sends right after the keypress
-    echo ''
-    [[ "$vm_fstab" =~ [^10] ]]
-do
-    :
-done
-if [[ $vm_fstab == 0 ]]; then
-  echo 'этап пропущен' 
-elif [[ $vm_fstab == 1 ]]; then
-nano /etc/fstab
-fi 
-clear
-echo " Установка завершена для выхода введите >> exit << "
-exit
 exit
